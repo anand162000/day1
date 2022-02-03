da276844be287ee8f723c91e9f807b75986bdd92
anand162000 <ananthakrishnanvs162000@gmil.com>
2022-02-03 11:14:58 +0530

    first commit

---
+++ src/day1/addSum.java
@@ -0,0 +1,14 @@
+package day1;
+import java.util.Scanner;
+public class addSum{
+	public static void main(String []args) {
+		int a,b,c;
+		Scanner sp=new Scanner(System.in);
+		a=sp.nextInt();
+		b=sp.nextInt();
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
+
